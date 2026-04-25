534d7aa16b79ae32b256a06b9b47d52d0d10f2c0
WilgotW <wilgotwardell@hotmail.com>
2026-04-25 12:08:33 +0200

    -

--- rubiks-bare-metal/bsp/src/uart_driver.c
+++ rubiks-bare-metal/bsp/src/uart_driver.c
@@ -6,6 +6,10 @@
 //STM pins: PB6: TX.  PB7: RX
 //Enable clock for Rx and Tx pin 
 
+//by viewing the block diagram, we can see that PORT B is connected to the AHB1 bus.
+//In the refrence manual, we can see that GPIOB has a offset of 1
 void setup(void) {
-    RCC->APB1ENR
+    RCC->AHB1ENR |= (1 << 1); //GPIOB now has clock access
+
+
 }
